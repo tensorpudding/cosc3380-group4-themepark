6dd24e659bcc722ea80eaccc2fbc982342cc7497
carisbec <117100389+carisbec@users.noreply.github.com>
2022-11-01 15:45:09 -0500

    Adding employee add form

---
+++ cosc3380-group4-themepark/Pages/AddEmployee.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cosc3380_group4_themepark;
+
+public class AddEmployeeModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
