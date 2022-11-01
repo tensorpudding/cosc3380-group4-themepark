7a29d99704c94efcd5d5315f06f25a1e9a13b343
nandoj28 <fjernandojrecinos@gmail.com>
2022-10-31 22:13:15 -0500

    Created Maintenance Form

---
+++ cosc3380-group4-themepark/Pages/Maintenance_Calendar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cosc3380_group4_themepark.Pages
+{
+    public class Maintenance_CalendarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
