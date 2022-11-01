fdc6d239d696cc622959a0e3221fdfa80d25f543
Michael Moorman <michael.l.moorman@protonmail.com>
2022-10-31 21:30:30 -0500

    Adding more files to main

---
+++ cosc3380-group4-themepark/Ticket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace cosc3380_group4_themepark.Models;
+
+public class Ticket
+{
+    [BindProperty]
+    public Int32 ticketID { get; set; }
+
+    [BindProperty]
+    public DateTime datetime { get; set; }
+
+    [BindProperty]
+    public String ticketClass { get; set; }
+
+    [BindProperty]
+    public decimal price { get; set; }
+
+    [BindProperty]
+    public Int32 reservation { get; set; }
+
+    public Ticket()
+    {
+    }
+}
+
