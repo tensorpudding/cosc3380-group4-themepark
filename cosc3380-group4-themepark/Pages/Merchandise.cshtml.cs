01d3ebeed16683ffaaca583ea2577f4ac02bd416
Joshua Kanagasabai <joshua@Joshuas-MacBook-Pro.local>
2022-11-03 18:01:34 -0500

    merchandise changes

---
+++ cosc3380-group4-themepark/Pages/Merchandise.cshtml.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using cosc3380_group4_themepark.Models;
+using cosc3380_group4_themepark;
+
+namespace cosc3380_group4_themepark.Pages
+{
+    public class MerchandiseModel : PageModel
+    {
+       
+        public void OnGet()
+        {
+
+        }
+        public void OnPostSubmit(MerchandiseSelection selection)
+        {
+
+
+   
+
+
+        }
+    }
+
+
+}
