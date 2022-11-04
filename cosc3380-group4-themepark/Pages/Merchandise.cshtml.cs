f1e9ed4dbe5a9f405dbf09a62fe8ede5b29f3fe9
Joshua Kanagasabai <joshua@Joshuas-MacBook-Pro.local>
2022-11-03 19:05:52 -0500

    html changes

--- cosc3380-group4-themepark/Pages/Merchandise.cshtml.cs
+++ cosc3380-group4-themepark/Pages/Merchandise.cshtml.cs
@@ -10,17 +10,46 @@ namespace cosc3380_group4_themepark.Pages
 {
     public class MerchandiseModel : PageModel
     {
+
+        
+
        
+
+
+        public List<MerchandiseRow> MyMerch { get; set; }
+
+
+
         public void OnGet()
         {
+            this.MyMerch = new List<MerchandiseRow>();
 
         }
-        public void OnPostSubmit(MerchandiseSelection selection)
+        public IActionResult OnPostSubmit(MerchandiseSelection selection)
         {
+            SqlDataReader reader = SqlHelper.ExecuteProcReader("Theme_Park.Merchandise_Report");
+
+            //create list of varaibles
+            this.MyMerch = new List<MerchandiseRow>();
 
 
-   
 
+
+            while (reader.Read())
+            {
+                MerchandiseRow current_Row = new MerchandiseRow();
+                current_Row.itemName = reader.GetString(0);
+                current_Row.numSold = reader.GetInt32(1);
+                current_Row.price = reader.GetDecimal(2);
+                current_Row.itemRevenue = reader.GetDecimal(3);
+                this.MyMerch.Add(current_Row);
+
+            }
+
+
+
+            return Page();
+            //display
 
         }
     }
