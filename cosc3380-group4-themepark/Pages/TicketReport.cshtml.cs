80d06ca4efb64cd7f8a33925ea2b5cc9dc153741
Michael Moorman <michael.l.moorman@protonmail.com>
2022-11-09 13:52:10 -0600

    Working on ticket report...trying JSON serialization

--- cosc3380-group4-themepark/Pages/TicketReport.cshtml.cs
+++ cosc3380-group4-themepark/Pages/TicketReport.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
 
 using cosc3380_group4_themepark.Models;
 using Microsoft.Data.SqlClient;
---
+++ cosc3380-group4-themepark/wwwroot/js/TicketReport.js
@@ -0,0 +1,9 @@
+﻿const displayCharts = () => {
+    let _ = null;
+    const mainChart1 = document.getElementById("main1-chart").getContext("2d");
+    const mainChart2 = document.getElementById("main2-chart").getContext("2d");
+    var thisYearNumbers = JSON.parse(document.currentScript.getAttribute("weekday_breakdown_this_year"));
+    var lastYearNumbers = JSON.parse(document.currentScript.getAttribute("weekday_breakdown_last_year"));
+
+
+}
