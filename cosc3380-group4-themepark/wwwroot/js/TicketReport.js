c711a8dabc2e6d9c80e6597099bdcadedc034abb
Michael Moorman <michael.l.moorman@protonmail.com>
2022-11-09 17:41:12 -0600

    Data is now nicer looking

--- cosc3380-group4-themepark/wwwroot/js/TicketReport.js
+++ cosc3380-group4-themepark/wwwroot/js/TicketReport.js
@@ -1,16 +1,23 @@
 ﻿const get_numbers_by_day_by_type = (yearNumbers, type) => {
     var classNums = [0, 0, 0, 0, 0, 0, 0];
     var i = 0;
-    yearNumbers["totals_by_weekday"].forEach(day => {
-        Object.entries(day).forEach(tclass => {
-            classNums[i] += tclass[1];
+    if (type == "All") {
+        yearNumbers["totals_by_weekday"].forEach(day => {
+            Object.entries(day).forEach(tclass => {
+                classNums[i] += tclass[1];
+            });
+            i++;
         });
-        i++;
-    });
+    } else {
+        yearNumbers["totals_by_weekday"].forEach(day => {
+            classNums[i] = day[type];
+            i++;
+        });
+    }
     i = 0;
     for (i = 0; i < 7; i++)
     {
-        console.log("Day " + i + " sales are " + classNums[i]);
+        console.log("Day " + i + " sales of type " + type + " are " + classNums[i]);
     }
     return classNums;
 };
@@ -27,17 +34,83 @@ const displayCharts = () => {
             type: 'bar',
             data: {
                 labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-                datasets: [{
+                datasets: [
+                    {
                     label: 'Poor ticket sales',
                     data: get_numbers_by_day_by_type(thisYearNumbers, 'Poor'),
                     backgroundColor: [
                         // 7 different colors?
+                        'rgba(0, 63, 92, 0.6)',
+                        'rgba(44, 72, 117, 0.6)',
+                        'rgba(138, 80, 143, 0.6)',
+                        'rgba(188, 80, 144, 0.6)',
+                        'rgba(255, 99, 97, 0.6)',
+                        'rgba(255, 133, 49, 0.6)',
+                        'rgba(255, 166, 0, 0.6)'
                     ],
                     borderColor: [
                         // 7 slightly different colors
+                        'rgba(0, 63, 92, 1)',
+                        'rgba(44, 72, 117, 1)',
+                        'rgba(138, 80, 143, 1)',
+                        'rgba(188, 80, 144, 1)',
+                        'rgba(255, 99, 97, 1)',
+                        'rgba(255, 133, 49, 1)',
+                        'rgba(255, 166, 0, 1)'
                     ],
                     borderWidth: 1
-                }]
+                    },
+                    {
+                        label: 'Normal ticket sales',
+                        data: get_numbers_by_day_by_type(thisYearNumbers, 'Normal'),
+                        backgroundColor: [
+                            // 7 different colors?
+                            'rgba(0, 63, 92, 0.6)',
+                            'rgba(44, 72, 117, 0.6)',
+                            'rgba(138, 80, 143, 0.6)',
+                            'rgba(188, 80, 144, 0.6)',
+                            'rgba(255, 99, 97, 0.6)',
+                            'rgba(255, 133, 49, 0.6)',
+                            'rgba(255, 166, 0, 0.6)'
+                        ],
+                        borderColor: [
+                            // 7 slightly different colors
+                            'rgba(0, 63, 92, 1)',
+                            'rgba(44, 72, 117, 1)',
+                            'rgba(138, 80, 143, 1)',
+                            'rgba(188, 80, 144, 1)',
+                            'rgba(255, 99, 97, 1)',
+                            'rgba(255, 133, 49, 1)',
+                            'rgba(255, 166, 0, 1)'
+                        ],
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Premium ticket sales',
+                        data: get_numbers_by_day_by_type(thisYearNumbers, 'Premium'),
+                        backgroundColor: [
+                            // 7 different colors?
+                            'rgba(0, 63, 92, 0.6)',
+                            'rgba(44, 72, 117, 0.6)',
+                            'rgba(138, 80, 143, 0.6)',
+                            'rgba(188, 80, 144, 0.6)',
+                            'rgba(255, 99, 97, 0.6)',
+                            'rgba(255, 133, 49, 0.6)',
+                            'rgba(255, 166, 0, 0.6)'
+                        ],
+                        borderColor: [
+                            // 7 slightly different colors
+                            'rgba(0, 63, 92, 1)',
+                            'rgba(44, 72, 117, 1)',
+                            'rgba(138, 80, 143, 1)',
+                            'rgba(188, 80, 144, 1)',
+                            'rgba(255, 99, 97, 1)',
+                            'rgba(255, 133, 49, 1)',
+                            'rgba(255, 166, 0, 1)'
+                        ],
+                        borderWidth: 1
+                    }
+                ]
             },
             options: {
                 scales: {
