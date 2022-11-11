0062691507677920a4c166804ec3a3df1de435dc
Michael Moorman <michael.l.moorman@protonmail.com>
2022-11-11 15:52:49 -0600

    Initial page for manager notifications

---
+++ cosc3380-group4-themepark/ManagerNotification.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace cosc3380_group4_themepark.Models
+{
+    public class ManagerNotification
+    {
+        [BindProperty]
+        public String employee_fname { get; set; }
+
+        [BindProperty]
+        public String employee_lname { get; set; }
+
+        [BindProperty]
+        public String dept_name { get; set; }
+
+        [BindProperty]
+        public String role { get; set; }
+
+        [BindProperty]
+        public Decimal payrate { get; set; }
+
+        [BindProperty]
+        public Boolean salaried { get; set; }
+
+        [BindProperty]
+        public String super_fname { get; set; }
+
+        [BindProperty]
+        public String super_lname { get; set; }
+
+        [BindProperty]
+        public Char type { get; set; }
+
+
+        public ManagerNotification()
+        {
+        }
+    }
+}
+
---
+++ cosc3380-group4-themepark/Pages/ManagerNotification.cshtml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+using cosc3380_group4_themepark.Models;
+
+namespace cosc3380_group4_themepark.Pages
+{
+    public class ManagerNotificationModel : PageModel
+    {
+        public List<ManagerNotification> myPayNotifications { get; set; }
+
+        public List<ManagerNotification> myRoleNotifications { get; set; }
+
+        public String username { get; set; }
+
+        public String? GetUsernameFromContext()
+        {
+            foreach (var claim in HttpContext.User.Claims)
+            {
+                if (claim.Type == "Username")
+                {
+                    return claim.Value;
+                }
+            }
+            return null;
+        }
+
+        public IActionResult OnGet()
+        {
+            String? username = GetUsernameFromContext();
+            if (username == null)
+                return Redirect("/");
+            else
+                this.username = username;
+            SqlDataReader reader = SqlHelper.ExecuteProcReader(
+                "[Theme_Park].[Proc_Get_Manager_Notifications]",
+                new SqlParameter("@username", this.username));
+
+            this.myPayNotifications = new List<ManagerNotification>();
+            this.myRoleNotifications = new List<ManagerNotification>();
+
+            while (reader.Read())
+            {
+                ManagerNotification notification = new ManagerNotification();
+                notification.employee_fname = reader.GetString(1);
+                notification.employee_lname = reader.GetString(2);
+                notification.dept_name = reader.GetString(3);
+                notification.role = reader.GetString(4);
+                notification.payrate = reader.GetDecimal(5);
+                notification.salaried = reader.GetBoolean(6);
+                notification.super_fname = reader.GetString(7);
+                notification.super_lname = reader.GetString(8);
+                Char type = reader.GetString(9)[0];
+                if (type == 'P')
+                {
+                    this.myPayNotifications.Add(notification);
+                }
+                else if (type == 'R')
+                {
+                    this.myRoleNotifications.Add(notification);
+                }
+            }
+
+            return Page();
+        }
+    }
+}
