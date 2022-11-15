98a978ab7f872ae86dd93cab6928a3506dbc09f2
carisbec <117100389+carisbec@users.noreply.github.com>
2022-11-15 17:08:38 -0600

    Finished employee entry form

--- cosc3380-group4-themepark/Departments.cs
+++ cosc3380-group4-themepark/Departments.cs
@@ -5,17 +5,18 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace cosc3380_group4_themepark.Models
 {
+
     public class Departments
     {
 
         [BindProperty]
-        public int dept_id { get; set; }
+        public Int32 dept_id { get; set; }
 
         [BindProperty]
         public String dept_name { get; set; }
 
         [BindProperty]
-        public int? supervisor_ssn { get; set; }
+        public Int32? supervisor_ssn { get; set; }
 
         public Departments()
         {
