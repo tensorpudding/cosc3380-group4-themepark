37fe73ddbaa5537190fef351ac7b24d4a318aa1e
Michael Moorman <michael.l.moorman@protonmail.com>
2022-11-26 17:27:52 -0600

    Adapted Attraction Report similarly to the other report to use date ranges

--- cosc3380-group4-themepark/Pages/AttractionReport.cshtml.cs
+++ cosc3380-group4-themepark/Pages/AttractionReport.cshtml.cs
@@ -13,10 +13,7 @@ using Microsoft.Data.SqlClient;
 namespace cosc3380_group4_themepark.Pages
 {
 
-    [Authorize(Policy="SalesSpecific")]
-    public class AttractionReportModel : PageModel
-    {
-        public class AttractionData
+            public class AttractionData
         {
             public String attraction_name { get; set; }
             public Int32 attraction_days_in_maintenance { get; set; }
@@ -27,19 +24,24 @@ namespace cosc3380_group4_themepark.Pages
             {
             }
         }
+    [Authorize(Policy="SalesSpecific")]
+    public class AttractionReportModel : PageModel
+    {
+
 
         public List<AttractionData> AttractionDataList { get; set; }
-        public List<Tuple<String, Int32>> top10 { get; set; }
+
+        public Int32 _year {get; set;}
 
 
-        public void GetAttractionReports(Int32 year)
+        public List<AttractionData> GetAttractionReportsByYear(Int32 year)
         {
             SqlDataReader reader = SqlHelper.ExecuteProcReader(
                     "[Theme_Park].[Proc_Attraction_Report_1]",
                     new SqlParameter("@year", year)
             );
 
-            this.AttractionDataList = new List<AttractionData>();
+            var AttractionDataList = new List<AttractionData>();
             while (reader.Read())
             {
                 AttractionData attraction = new AttractionData();
@@ -48,8 +50,49 @@ namespace cosc3380_group4_themepark.Pages
                 attraction.attraction_days_in_maintenance = reader.GetInt32(2);
 
                 attraction.attraction_rides_average_this_season = reader.GetDecimal(3);
-                this.AttractionDataList.Add(attraction);
+                AttractionDataList.Add(attraction);
             }
+
+            return AttractionDataList;
+        }
+
+        public List<AttractionData> GetAttractionReportsByRange(DateTime startdate, DateTime enddate)
+        {
+            SqlDataReader reader = SqlHelper.ExecuteProcReader(
+                "[Theme_Park].[Proc_Attraction_Report_3]",
+                new SqlParameter("@startdate", startdate),
+                new SqlParameter("@enddate", enddate)
+            );
+
+            var AttractionDataList = new List<AttractionData>();
+            while (reader.Read())
+            {
+                AttractionData attraction = new AttractionData();
+                attraction.attraction_name = reader.GetString(0);
+                attraction.attraction_rides = reader.GetInt32(1);
+                attraction.attraction_days_in_maintenance = reader.GetInt32(2);
+
+                attraction.attraction_rides_average_this_season = reader.GetDecimal(3);
+                AttractionDataList.Add(attraction);
+            }
+
+            return AttractionDataList;
+        }
+
+        public PartialViewResult OnGetAttractionReport(String checkyearorrange, Int32? year, String? startdate, String? enddate)
+        {
+            this.AttractionDataList = new List<AttractionData>();
+            Console.WriteLine("Debug test OnPostGenerateItemized");
+            if (checkyearorrange == "year")
+            {
+                this.AttractionDataList = GetAttractionReportsByYear(year.GetValueOrDefault());
+            }
+            else if (checkyearorrange == "range")
+            {
+                this.AttractionDataList = GetAttractionReportsByRange(DateTime.Parse(startdate), 
+                                                     DateTime.Parse(enddate));
+            }
+            return Partial("_AttractionReport", this.AttractionDataList);
         }
 
         public void OnGet(Int32? year)
@@ -63,8 +106,9 @@ namespace cosc3380_group4_themepark.Pages
             {
                 year_value = Convert.ToInt32(year);
             }
+            this._year = year_value;
             Console.WriteLine(year_value);
-            GetAttractionReports(year_value);
+            GetAttractionReportsByYear(year_value);
         }
 
         public IActionResult OnPostChangeYear(Int32 year)
--- cosc3380-group4-themepark/wwwroot/js/AttractionReport.js
+++ cosc3380-group4-themepark/wwwroot/js/AttractionReport.js
@@ -1,7 +1,64 @@
-window.addEventListener("load", (event) =>
+// window.addEventListener("load", (event) =>
+// {
+//     const params = new URLSearchParams(window.location.search);
+//     const year = params.get("year");
+//     let option = document.getElementById(year);
+//     option.defaultSelected = true;
+// })
+
+const disableItemizedForm = () =>
 {
-    const params = new URLSearchParams(window.location.search);
-    const year = params.get("year");
-    let option = document.getElementById(year);
-    option.defaultSelected = true;
+    byYearCheck = document.getElementsByName("checkyearorrange")[0];
+    byRangeCheck = document.getElementsByName("checkyearorrange")[1];
+    startDateInput = document.getElementById("reportStartDate");
+    endDateInput = document.getElementById("reportEndDate");
+    yearInput = document.getElementById("reportYear");
+    if (byYearCheck.checked)
+    {
+        console.log("We have it set by year");
+        startDateInput.disabled = true;
+        startDateInput.required = false;
+        endDateInput.disabled = true;
+        endDateInput.required = false;
+        yearInput.disabled = false;
+        yearInput.required = true;
+    }
+    else
+    {
+        console.log("We have it set by range");
+        startDateInput.disabled = false;
+        startDateInput.required = true;
+        endDateInput.disabled = false;
+        endDateInput.required = true;
+        yearInput.disabled = true;
+        yearInput.required = false;
+    }
+}
+
+document.getElementsByName("checkyearorrange")[0].addEventListener("change", disableItemizedForm);
+document.getElementsByName("checkyearorrange")[1].addEventListener("change", disableItemizedForm);
+
+document.getElementById("generate-report-button").addEventListener('click', () => {
+    let check = document.getElementsByName("checkyearorrange")[0].checked;
+    var choice;
+    if (check)
+    {
+        choice = "year";
+    }
+    else
+    {
+        choice = "range"
+    }
+    let year = document.getElementById("reportYear").value;
+    let startdate = document.getElementById("reportStartDate").value;
+    let enddate = document.getElementById("reportEndDate").value;
+    let partialString = "/AttractionReport?handler=AttractionReport&checkyearorrange=" + choice + "&year=" + year + "&startdate=" + startdate + "&enddate=" + enddate;
+    console.log(partialString);
+    fetch(partialString)
+        .then((response) => {
+            return response.text();
+        })
+        .then((result) => {
+            document.getElementById("attraction-report-container").innerHTML = result;
+        });
 })
